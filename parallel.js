0ee309c2610cd0dc32db53188245c36636af592c
Adam Savitzky <adam.savitzky@gmail.com>
2013-02-13 21:00:21 -0800

    Sublime decided to put tabs instead of spaces.

--- parallel.js
+++ parallel.js
@@ -1,104 +1,104 @@
 /*
- *	Library: parallel.js
- *	Author: Adam Savitzky
+ *  Library: parallel.js
+ *  Author: Adam Savitzky
  *  License: Creative Commons 3.0
  */
 
 var Parallel = (function  () {
 
-	var spawn = (function () {
+    var spawn = (function () {
 
-		var wrap = function (fn) {
-			return 'self.onmessage = function (e) { self.postMessage(JSON.stringify((' + fn.toString() + ').apply(self, JSON.parse(e.data)))); }';
-		};
+        var wrap = function (fn) {
+            return 'self.onmessage = function (e) { self.postMessage(JSON.stringify((' + fn.toString() + ').apply(self, JSON.parse(e.data)))); }';
+        };
 
-		var RemoteRef = function (fn) {
-			var str = wrap(fn),
-				blob = new Blob([str], { type: 'text/javascript' }),
-				url = URL.createObjectURL(blob),
-				worker = new Worker(url);
+        var RemoteRef = function (fn) {
+            var str = wrap(fn),
+                blob = new Blob([str], { type: 'text/javascript' }),
+                url = URL.createObjectURL(blob),
+                worker = new Worker(url);
 
-			worker.onmessage = this.onWorkerMsg;
+            worker.onmessage = this.onWorkerMsg;
 
-			this.worker = worker;
-			this.worker.ref = this;
-		};
+            this.worker = worker;
+            this.worker.ref = this;
+        };
 
-		RemoteRef.prototype.onWorkerMsg = function (e) {
-			this.ref.data = JSON.parse(e.data);
-		};
+        RemoteRef.prototype.onWorkerMsg = function (e) {
+            this.ref.data = JSON.parse(e.data);
+        };
 
-		RemoteRef.prototype.data = undefined;
+        RemoteRef.prototype.data = undefined;
 
-		RemoteRef.prototype.fetch = function (cb) {
-			if (this.data === '___terminated') {
-				return;
-			}
+        RemoteRef.prototype.fetch = function (cb) {
+            if (this.data === '___terminated') {
+                return;
+            }
 
-			return this.data ? (cb ? cb(this.data): this.data) : (setTimeout(_.bind(this.fetch, this, cb), 0) && undefined);
-		};
+            return this.data ? (cb ? cb(this.data): this.data) : (setTimeout(_.bind(this.fetch, this, cb), 0) && undefined);
+        };
 
-		RemoteRef.prototype.terminate = function () {
-			this.data = '___terminated';
-			this.worker.terminate();
-		};
+        RemoteRef.prototype.terminate = function () {
+            this.data = '___terminated';
+            this.worker.terminate();
+        };
 
-		return function (fn, args) {
-			var r = new RemoteRef(fn);
-			r.worker.postMessage(JSON.stringify([].concat(args)));
+        return function (fn, args) {
+            var r = new RemoteRef(fn);
+            r.worker.postMessage(JSON.stringify([].concat(args)));
 
-			return r;
-		};
+            return r;
+        };
 
-	})();
+    })();
 
-	var mapreduce = (function () {
+    var mapreduce = (function () {
 
-		var DistributedProcess = function (mapper, reducer, chunks) {
-			this.mapper = mapper;
-			this.reducer = reducer;
-			this.chunks = chunks;
-			this.refs = _.map(chunks, function (chunk) {
-				return spawn(mapper, [].concat(chunk));
-			});
-		};
+        var DistributedProcess = function (mapper, reducer, chunks) {
+            this.mapper = mapper;
+            this.reducer = reducer;
+            this.chunks = chunks;
+            this.refs = _.map(chunks, function (chunk) {
+                return spawn(mapper, [].concat(chunk));
+            });
+        };
 
-		DistributedProcess.prototype.fetch = function (cb) {
-			var results = this.fetchRefs(),
-				that = this;
+        DistributedProcess.prototype.fetch = function (cb) {
+            var results = this.fetchRefs(),
+                that = this;
 
-			if (_.isEqual(results, _.without(results, undefined))) {
-				return cb ? cb(_.reduce(results, this.reducer)) : _.reduce(results, this.reducer);
-			}
+            if (_.isEqual(results, _.without(results, undefined))) {
+                return cb ? cb(_.reduce(results, this.reducer)) : _.reduce(results, this.reducer);
+            }
 
-			setTimeout(function () {
-				that.fetch(cb);
-			}, 100);
-		};
+            setTimeout(function () {
+                that.fetch(cb);
+            }, 100);
+        };
 
 
-		DistributedProcess.prototype.fetchRefs = function (cb) {
-			return _.map(this.refs, function (ref) {
-				return ref.fetch(cb || undefined);
-			}, this);
-		};
+        DistributedProcess.prototype.fetchRefs = function (cb) {
+            return _.map(this.refs, function (ref) {
+                return ref.fetch(cb || undefined);
+            }, this);
+        };
 
-		DistributedProcess.prototype.terminate = function (n) {
-			n !== undefined ? this.refs[n].terminate() : _.invoke(this.refs, 'terminate');
-		};
+        DistributedProcess.prototype.terminate = function (n) {
+            n !== undefined ? this.refs[n].terminate() : _.invoke(this.refs, 'terminate');
+        };
 
-		return function (mapper, reducer, chunks, cb) {
-			var d = new DistributedProcess(mapper, reducer, chunks);
+        return function (mapper, reducer, chunks, cb) {
+            var d = new DistributedProcess(mapper, reducer, chunks);
 
-			d.fetch(cb);
+            d.fetch(cb);
 
-			return d;
-		}
-	})();
+            return d;
+        }
+    })();
 
-	return {
-		mapreduce: mapreduce,
-		spawn: spawn
-	};
+    return {
+        mapreduce: mapreduce,
+        spawn: spawn
+    };
 
 })();
