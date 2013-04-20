2ba301a161e1ff59b0ca54c304e82031e9e9edc0
Sebastian Mayr <sebmaster16@gmail.com>
2013-04-20 18:43:42 +0200

    Adds performance test.

---
+++ test/performance.spec.js
@@ -0,0 +1,50 @@
+﻿describe('Performance', function () {
+	var isNode = typeof module !== 'undefined' && module.exports;
+	var cpus = isNode && require('os').cpus().length > 2 ? 2 : 1;
+
+	it('.map() should be using multi-threading (could fail on single-core)', function () {
+		var slowSquare = function (n) {
+			var i = 0;
+			while (++i < n * n) { }
+			return i;
+		};
+
+		var Parallel = require('../lib/parallel.js');
+		var p = new Parallel([1000, 2000, 3000]);
+		var p2 = new Parallel([1000, 2000, 3000]);
+
+		var start = Date.now();
+		var start2 = Date.now();
+
+		var time = null;
+		var time2 = null;
+
+		runs(function () {
+			p.spawn(function (data) {
+				for (var i = 0; i < data.length; ++i) {
+					var n = data[i];
+					var i = 0;
+					while (++i < n * n) { }
+					data[i] = i;
+				}
+				return data;
+			}).then(function (data) {
+				time = Date.now() - start;
+				result = data;
+			});
+
+			p2.map(slowSquare).then(function (data) {
+				time2 = Date.now() - start2;
+				result = data;
+			});
+		});
+
+		waitsFor(function () {
+			return time != null && time2 != null;
+		}, "it should finish", 5000);
+
+		runs(function () {
+			expect(time2).toBeLessThan(time / cpus);
+		});
+	});
+});
