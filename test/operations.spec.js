eb1a3a37fd84e392c56dc295b16dfd881bb5b171
Sebastian Mayr <sebmaster16@gmail.com>
2013-04-30 10:47:03 +0200

    Fix reduce problems.
    The point to resolve the promise got determined incorrectly.

---
+++ test/operations.spec.js
@@ -0,0 +1,24 @@
+﻿describe('Operations', function () {
+    var isNode = typeof module !== 'undefined' && module.exports;
+    var Parallel = isNode ? require('../lib/parallel.js') : self.Parallel;
+
+    it('should require(), map() and reduce correctly (check console errors)', function () {
+        var p = new Parallel([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+
+        function add(d) { return d[0] + d[1]; }
+        function factorial(n) { return n < 2 ? 1 : n * factorial(n - 1); }
+
+        p.require(factorial);
+
+        var done = false;
+        runs(function () {
+            p.map(function (n) { return Math.pow(10, n); }).reduce(add).then(function() {
+                done = true;
+            });
+        });
+
+        waitsFor(function () {
+            return done;
+        }, "it should finish", 500);
+    });
+});
