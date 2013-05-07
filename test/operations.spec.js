d2eff23783a846e1b1bc0ab0ba77352ebdbbebf8
Sebastian Mayr <sebmaster16@gmail.com>
2013-05-08 00:35:09 +0200

    Make test in IE pass

--- test/operations.spec.js
+++ test/operations.spec.js
@@ -3,7 +3,7 @@
     var Parallel = isNode ? require('../lib/parallel.js') : self.Parallel;
 
     it('should require(), map() and reduce correctly (check console errors)', function () {
-        var p = new Parallel([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    	var p = new Parallel([0, 1, 2, 3, 4, 5, 6, 7, 8], { evalPath: isNode ? undefined : 'lib/eval.js' });
 
         function add(d) { return d[0] + d[1]; }
         function factorial(n) { return n < 2 ? 1 : n * factorial(n - 1); }
