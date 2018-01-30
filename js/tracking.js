3fbbfa36497ca288e132392718bba68b520893c1
MaX Falstein <MaXFalstein@users.noreply.github.com>
2018-01-30 16:31:38 +0000

    Create tracking.js

---
+++ js/tracking.js
@@ -0,0 +1,6 @@
+var _gaq = [['_setAccount', 'UA-38199510-2'], ['_trackPageview']];
+(function (d, t) {
+  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g, s)
+}(document, 'script'));
